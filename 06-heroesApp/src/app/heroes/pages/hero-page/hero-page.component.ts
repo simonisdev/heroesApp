e0f8c33a00781cc79ff338423ec15f220bea7217
simonisdev <sagredo83@gmail.com>
2023-07-11 20:59:07 -0400

    Obtener Héroe por URL

--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +11,26 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private HeroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.HeroesService.getHeroById( id )),
+      ).subscribe( hero => {
+         if ( !hero ) return this.router.navigate([ '/heroes/list' ]);
+
+         this.hero = hero;
+         return;
+      })
+  }
 
 }
--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { enviaronments } from 'src/environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -13,6 +13,14 @@ export class HeroesService {
 
   getHeroes():Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`)
+  }
+
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+    .pipe(
+      catchError( error => of(undefined) )
+    );
+
   }
 
 }
