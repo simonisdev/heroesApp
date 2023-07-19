4e1243f91e964102e55e5cfd95db66ec86eacaa4
simonisdev <sagredo83@gmail.com>
2023-07-18 23:08:24 -0400

    HeroForm - ReactiveForms

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Publisher } from '../../interfaces/hero.interface';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-page',
@@ -8,6 +9,16 @@ import { Publisher } from '../../interfaces/hero.interface';
   ]
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id:         new FormControl<string>(''),
+    superhero:  new FormControl<string>('', { nonNullable: true }),
+    publisher:  new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:  new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img:    new FormControl(''),
+  });
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
