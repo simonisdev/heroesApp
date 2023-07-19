1bfc263c07107d01f9c423b4ba81c860abfd5578
simonisdev <sagredo83@gmail.com>
2023-07-18 23:28:41 -0400

    Conectar el formulario con el template

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -25,4 +25,17 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+
+  onSubmit():void {
+
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    })
+
+  }
+
+
+
+
 }
