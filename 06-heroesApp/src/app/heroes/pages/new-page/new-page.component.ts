c3205ada0e4fd57654e362e28e6c1406bffbca0a
simonisdev <sagredo83@gmail.com>
2023-07-20 03:28:02 -0400

    Getters dentro del componente

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -25,13 +26,20 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor( private heroesService: HeroesService  ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
 
   onSubmit():void {
 
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value,
-    })
+    // if ( this.heroForm.invalid ) return;
+
+
+
 
   }
 
